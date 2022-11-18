02e84ec883d1057a37013aab4de77e749c165107
takuzo8118 <saitou2930b@gmail.com>
2022-11-18 19:49:33 +0900

    Breeze認証機能インストールOK

--- resources/views/adminlte.blade.php
+++ resources/views/adminlte.blade.php
@@ -1,21 +1,28 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', '投稿一覧')
 
 @section('content_header')
     <h1>ダッシュボード</h1>
 @stop
 
 @section('content')
-    <p>ここがコンテンツ部分です</p>
+<div class="container text-center">
+    <ul>
+        <li class=""><a href="#">投稿1</a></li>
+        <li class=""><a href="#">投稿2</a></li>
+        <li class=""><a href="#">投稿3</a></li>
+    </ul>
+</div>
 @stop
 
 @section('css')
+    <style>
+        ul li {
+            list-style: none;
+        }
+    </style>
     {{-- ページごとCSSの指定
     <link rel="stylesheet" href="/css/xxx.css">
     --}}
-@stop
-
-@section('js')
-    <script> console.log('ページごとJSの記述'); </script>
 @stop
